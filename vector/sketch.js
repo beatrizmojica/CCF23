239eaa5dd63b626c5a5f054fdf522b2f752fd2d7
beatrizmojica <144291797+beatrizmojica@users.noreply.github.com>
2023-11-08 21:29:26 -0500

    sketch.js

--- vector/sketch.js
+++ vector/sketch.js
@@ -1,64 +1,35 @@
-
-let sphere;
-let face;
 let pos;
 let speed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  sphere = new Ball();
-  pos = createVector(width/2, height);
+  pos = createVector(width/2, height/2);
   speed = createVector(0, 0);
-  speed.x = random(-2,2);
-  speed.y = random(-2,2);
+  speed.x = random(3,10);
+  speed.y = random(3,10);
 }
 
 function draw() {
   background(200,60); // motion blur
-  sphere.update();
-  sphere.wallCheck();
-  sphere.render();
- 
-}
+  pos.add(speed);
+
+  if(pos.x >= width - 50 || pos.x <= 50){
+    speed.x *= -1;
+      fill(random(255), random(255), random(255));
+      ellipse(pos.x, pos.y, 300); // ellipse flash
+    }
   
-class Ball {
+     if(pos.y >= height - 50 || pos.y <= 50){
+    speed.y *= -1;
+       fill(random(255), random(255), random(255)); 
+       ellipse(pos.x,pos.y, 300); //ellipse flash
+     }
 
-  constructor() {
-
-    this.diam = 180;
-
-    this.xpos = width/2;
-    this.ypos = height/2;
-
-    this.xspeed = random(3, 10);
-    this.yspeed = random(3, 10);
-  }
-
-  update(){
-    this.xpos += this.xspeed
-    this.ypos += this.yspeed
-  }
-
-  wallCheck(){
-    if(this.xpos >= width - this.diam/2 || this.xpos <= this.diam/2){
-  this.xspeed = this.xspeed * -1;
-    fill(random(255), random(255), random(255));
-    ellipse(this.xpos, this.ypos, this.diam+100); // ellipse flash
-  }
-
-   if(this.ypos >= height - this.diam/2 || this.ypos <= this.diam/2){
-  this.yspeed = this.yspeed * -1;
-     fill(random(255), random(255), random(255)); 
-     ellipse(this.xpos,this.ypos, this.diam + 100); //ellipse flash
-   }
-  }
-
-render(){
   strokeWeight(1);
-  ellipse(this.xpos, this.ypos, this.diam);
+  ellipse(pos.x, pos.y, 200);
   strokeWeight(5);
-  ellipse(this.xpos-40, this.ypos-40, 10);
-  ellipse(this.xpos + 40, this.ypos - 40, 10);
-  bezier(this.xpos-50, this.ypos, this.xpos-40, this.ypos+60, this.xpos+40, this.ypos+60, this.xpos+50, this.ypos);
-}
-}
+  ellipse(pos.x-40, pos.y-40, 10);
+  ellipse(pos.x + 40, pos.y - 40, 10);
+  bezier(pos.x-50, pos.y, pos.x-40, pos.y+60, pos.x+40, pos.y+60, pos.x+50, pos.y);
+
+    }
